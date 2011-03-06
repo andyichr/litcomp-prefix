9ee69faef75f076debc9e2d47ba791d4688e0f62
Andy Christianson <git@andyic.org>
2011-03-06 16:05:41 -0500

    added pkg_version

--- src/sh/env.sh
+++ src/sh/env.sh
@@ -29,19 +29,9 @@ prefixstr()
 
 export -f prefixstr
 
-prep_latest()
+pkg_version()
 {
-	PROJ_BASE_URL="$2"
-	PROJ_FILE=$( curl -s $PROJ_BASE_URL | grep -o "$3" | tail -n1 )
-	PROJ_VERSION=$( echo "$PROJ_FILE" | sed -e "s/$4$//" )
-	PROJ_URL="${PROJ_BASE_URL}${PROJ_FILE}"
-	if [ ! -e "$PROJ_FILE" ]; then
-		echo "Retrieving: $PROJ_URL"
-		curl "$PROJ_URL" -o "$PROJ_FILE.tmp"
-		mv "$PROJ_FILE.tmp" "$PROJ_FILE"
-	fi
-	tar xf "$PROJ_FILE"
-	pushd $( find . -maxdepth 1 -type d -name "$1*" | head -n1 )
+	cat "$SRCDIR"/src/sh/version.d/$1
 }
 
-export -f prep_latest
+export -f pkg_version
