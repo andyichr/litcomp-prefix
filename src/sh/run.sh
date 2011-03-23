141164be0667a94c28cc76fe9b2889202ba52891
Andy Christianson <git@andyic.org>
2011-03-22 20:55:23 -0400

    added invocation of litcomp to make run target

--- src/sh/run.sh
+++ src/sh/run.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 ./src/sh/make.sh "$(pwd)/.prefix"
-export PATH="\"$(pwd)/.prefix/bin\":$PATH"
+export PATH="$(pwd)/.prefix/bin:$PATH"
 export LD_LIBRARY_PATH="$(pwd)/.prefix/lib"
-./litcomp/bin/litcompd ./litcomp ./litcomp/test/wiki
+pushd litcomp
+	ant dist
+	make
+popd
+./litcomp/bin/litcompd "$(pwd)"/litcomp "$(pwd)"/litcomp/test/wiki
