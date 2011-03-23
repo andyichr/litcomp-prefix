f9ce998ae63ce818e224e429b2337e833065c7e7
Andy Christianson <git@andyic.org>
2011-03-22 20:39:57 -0400

    added nodejs and openssl

---
+++ src/sh/make.d/100/nodejs.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+test -d nodejs || {
+	BUILD_DIR="$( pwd )"
+	test -d nodejs.tmp && rm -rf nodejs.tmp
+	mkdir nodejs.tmp && cd nodejs.tmp
+	curl http://nodejs.org/dist/node-v$(pkg_version nodejs).tar.gz | gunzip | tar x
+	cd "$BUILD_DIR" && mv nodejs.tmp nodejs
+}
+
+cd nodejs/node-v$(pkg_version nodejs)
+
+./configure --prefix="$PREFIX" --openssl-includes="$PREFIX"/include --openssl-libpath="$PREFIX"/lib
+make
+make install
---
+++ src/sh/make.d/50/openssl.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+test -d openssl || {
+	BUILD_DIR="$( pwd )"
+	test -d openssl.tmp && rm -rf openssl.tmp
+	mkdir openssl.tmp && cd openssl.tmp
+	curl http://www.openssl.org/source/openssl-$(pkg_version openssl).tar.gz | gunzip | tar x
+	cd "$BUILD_DIR" && mv openssl.tmp openssl
+}
+
+cd openssl/openssl-$(pkg_version openssl)
+
+./config --prefix="$PREFIX" shared
+make
+make install
