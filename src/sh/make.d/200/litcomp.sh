b814c141dca26f15550c73ef2dc2ac5a5facd585
Andy Christianson <git@andyic.org>
2011-03-30 20:49:08 -0400

    removed debugging statement

--- src/sh/make.d/200/litcomp.sh
+++ src/sh/make.d/200/litcomp.sh
@@ -7,6 +7,5 @@ cd ../litcomp
 
 export CXX_FLAGS="-I$PREFIX/include -L$PREFIX/lib"
 #export TINYXML_LIB="$PREFIX/lib/libtinyxml.a"
-echo $TINYXML_LIB
 #export TINYXML_LIB="$(find $SRCDIR/.build/tinyxml/tinyxml -name "*.o" -not -name "*test*" | tr "\n" " ")"
 make
