afba1e8e868e33edd35aa515861231b57c9c0ccf
Andy Christianson <andy@andyic.org>
2011-04-23 15:13:30 -0400

    added node-openid, npm, and tweaked node install to support npm modules

--- src/sh/make.d/100/nodejs.sh
+++ src/sh/make.d/100/nodejs.sh
@@ -16,3 +16,14 @@ cd nodejs/node-v$(pkg_version nodejs)
 ./configure --prefix="$PREFIX" --openssl-includes="$PREFIX"/include --openssl-libpath="$PREFIX"/lib
 make
 make install
+
+mv "$PREFIX/bin/node" "$PREFIX/bin/node.orig"
+
+cat <<EOF > "$PREFIX/bin/node"
+#!/usr/bin/env bash
+
+export NODE_PATH="$PREFIX/lib/node_modules:\$NODE_PATH"
+exec node.orig "\$@"
+EOF
+
+chmod +x "$PREFIX/bin/node"
--- src/sh/make.d/200/npm.sh
+++ src/sh/make.d/200/npm.sh
@@ -3,4 +3,16 @@
 set -x
 set -e
 
+export HOME="$PREFIX/lib/npm"
 curl 'http://npmjs.org/install.sh' | npm_install=rc sh
+
+mv "$PREFIX/bin/npm" "$PREFIX/bin/npm.orig"
+
+cat <<EOF > "$PREFIX/bin/npm"
+#!/usr/bin/env bash
+
+export HOME="$PREFIX/lib/npm"
+npm.orig "\$@"
+EOF
+
+chmod +x "$PREFIX/bin/npm"
