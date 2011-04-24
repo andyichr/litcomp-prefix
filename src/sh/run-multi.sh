8e34fd63ff063e3e2e05cff12e6affa38baf7a61
Andy Christianson <andy@andyic.org>
2011-04-24 14:14:34 -0400

    initial commit

---
+++ src/sh/run-multi.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+set -e
+
+./src/sh/make.sh "$(pwd)/.prefix"
+export PATH="$(pwd)/.prefix/bin:$PATH"
+export LD_LIBRARY_PATH="$(pwd)/.prefix/lib"
+export LITCOMP_HOME="$(pwd)/litcomp"
+
+cd litcomp-multi
+
+case "$1" in
+TEST)
+	make test
+	true
+	;;
+DOCS)
+	make docs
+	true
+	;;
+*)
+	./bin/litcomp-multi "$(pwd)" <(cat <<EOF
+{
+	"litcomp-multi": {
+		"base-url": "http://127.0.0.1:8071",
+		"data": {
+			"provider": {
+				"name": "filesystem",
+				"config": {
+					"path": "$(pwd)/.prefix/var/litcomp-multi"
+				}
+			}
+		}
+	},
+	"litcomp": {
+		"home": "$LITCOMP_HOME",
+		"data": {
+			"provider": {
+				"name": "git",
+				"config": {
+					"repository": "git://..."
+				}
+			}
+		}
+	}
+}
+EOF
+)
+	;;
+esac
